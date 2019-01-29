9ff90b0bce10ddf51db82fe07b51ae103bbd69c4
pnam99 <43445638+pnam99@users.noreply.github.com>
2019-01-29 17:12:24 -0500

    First Commit (Ch.3 Demo)

---
+++ app/src/main/java/edu/quinnipiac/ser210/mymessenger/ReceiveMessageActivity.java
@@ -0,0 +1,23 @@
+package edu.quinnipiac.ser210.mymessenger;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class ReceiveMessageActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "message";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive_message);
+        Intent intent = getIntent();
+        String messageText = ((Intent) intent).getStringExtra(EXTRA_MESSAGE);
+        TextView messageView = (TextView) findViewById(R.id.message);
+        messageView.setText(messageText);
+    }
+}
